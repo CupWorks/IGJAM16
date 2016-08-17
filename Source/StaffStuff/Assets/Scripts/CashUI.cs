50d1209e69c987041a51f4965f5d9fe9333e417a
Jan-Hendrik <jhkahle@hotmail.de>
2016-08-17 22:46:15 +0200

    ADDED: Income UI

---
+++ Source/StaffStuff/Assets/Scripts/CashUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CashUI : MonoBehaviour
+{
+    private Text earnText;
+
+
+	private void Start ()
+    {
+        earnText = GetComponent<Text>();
+	}
+	
+	private void Update ()
+    {
+        earnText.text = GameSession.Instance.totalIncome.ToString("0.00") + " €";
+	}
+}

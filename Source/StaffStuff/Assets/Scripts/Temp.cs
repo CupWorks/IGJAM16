58228aa9e96e4841b80c347eea462e1f17b10e3d
Christoph Becher <christoph.becher@live.de>
2016-08-17 16:51:47 +0200

    Added main scene

--- Source/StaffStuff/Assets/Scripts/Temp.cs
+++ Source/StaffStuff/Assets/Scripts/Temp.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Temp : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 	
 	}
 }

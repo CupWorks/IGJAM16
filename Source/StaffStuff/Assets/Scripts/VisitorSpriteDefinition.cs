0ee04b905d141a8e1a761eeb2511ecada8ad2d06
Christoph Becher <christoph.becher@live.de>
2016-08-18 16:28:59 +0200

    Updated sprites

---
+++ Source/StaffStuff/Assets/Scripts/VisitorSpriteDefinition.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class VisitorSpriteDefinition
+{
+	public Sprite up;
+	public Sprite down;
+	public Sprite left;
+	public Sprite right;
+}
